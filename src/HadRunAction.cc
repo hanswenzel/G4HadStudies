b62a89e4567eeeb999d1903d1e2e60864688f29b
Hans-Joachim Wenzel <wenzel@fnal.gov>
2017-08-07 13:00:37 -0500

    add printing out of the cross section store

--- src/HadRunAction.cc
+++ src/HadRunAction.cc
@@ -72,6 +72,90 @@ void HadRunAction::EndOfRunAction(const G4Run*) {
     HadAnalysis* analysis = HadAnalysis::getInstance();
     analysis->SetRunActInfo(nEvts);
     analysis->finish();
+    G4String fElementName = "H";
+    G4String fParticleName = "pi-";
+    G4double fMinKinEnergy;
+    G4double fMaxKinEnergy;
+    G4double fMinMomentum;
+    G4double fMaxMomentum;
+
+    G4int fVerbose;
+    G4int fBinsE;
+    G4int fBinsP;
+    fMinKinEnergy = 0.1 * MeV;
+    fMaxKinEnergy = 10 * TeV;
+    fMinMomentum = 1 * MeV;
+    fMaxMomentum = 10 * TeV;
+
+    fBinsE = 800;
+    fBinsP = 700;
+    const G4Element* elm =
+            G4NistManager::Instance()->FindOrBuildElement(fElementName);
+    const G4Material* mat =
+            G4NistManager::Instance()->FindOrBuildMaterial("G4_" + fElementName);
+    const G4ParticleDefinition* particle =
+            G4ParticleTable::GetParticleTable()->FindParticle(fParticleName);
+
+    G4cout << "### Fill Cross Sections for " << fParticleName
+            << " off " << fElementName
+            << G4endl;
+    G4cout << "-------------------------------------------------------------"
+            << G4endl;
+    G4cout << "    N     E(MeV)   Elastic(b)   Inelastic(b)";
+    G4cout << "   Total(b)" << G4endl;
+    G4cout << "-------------------------------------------------------------"
+            << G4endl;
+    if (!particle || !elm) {
+        G4cout << "HistoManager WARNING Particle or element undefined" << G4endl;
+        return;
+    }
+
+    G4int prec = G4cout.precision();
+    G4cout.precision(4);
+
+    G4HadronicProcessStore* store = G4HadronicProcessStore::Instance();
+    G4double mass = particle->GetPDGMass();
+
+    // Build histograms
+
+    G4double p1 = std::log10(fMinMomentum / GeV);
+    G4double p2 = std::log10(fMaxMomentum / GeV);
+    G4double e1 = std::log10(fMinKinEnergy / MeV);
+    G4double e2 = std::log10(fMaxKinEnergy / MeV);
+    G4double de = (e2 - e1) / G4double(fBinsE);
+    G4double dp = (p2 - p1) / G4double(fBinsP);
+
+    G4double x = e1 - de * 0.5;
+    G4double e, p, xs, xtot;
+    G4int i;
+    const G4Material* fTargetMaterial;
+    fTargetMaterial = G4NistManager::Instance()->FindOrBuildMaterial("G4_H");
+    G4double coeff = 1.0;
+    if (fTargetMaterial) {
+        coeff = fTargetMaterial->GetDensity() * cm2 / g;
+    }
+
+    for (i = 0; i < fBinsE; i++) {
+        x += de;
+        e = std::pow(10., x) * MeV;
+        G4cout << std::setw(5) << i << std::setw(12) << e;
+        xs = store->GetElasticCrossSectionPerAtom(particle, e, elm, mat);
+        xtot = xs;
+        G4cout << std::setw(12) << xs / barn;
+        xs = store->GetInelasticCrossSectionPerAtom(particle, e, elm, mat);
+         G4cout << std::setw(12) << xs / barn;
+        xtot += xs;
+        G4cout << " " << std::setw(12) << xtot/barn << G4endl;  
+        if (fTargetMaterial) {
+            xs =
+                    store->GetInelasticCrossSectionPerVolume(particle, e, fTargetMaterial);
+            xs =
+                    store->GetElasticCrossSectionPerVolume(particle, e, fTargetMaterial);
+        }
+
+    }
+  G4cout << "-------------------------------------------------------------" 
+           << G4endl;
 
 }
 
