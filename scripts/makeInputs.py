4a2cc4576e433290ddea1ef1eb94ad7d441431a2
Hans-Joachim Wenzel <wenzel@fnal.gov>
2017-08-07 12:57:26 -0500

    add dealing with command line options

--- scripts/makeInputs.py
+++ scripts/makeInputs.py
@@ -1,34 +1,67 @@
-particle = "Kaon"
-g4particle = "kaon+"
-plist = "QGSP_BERT"
-energies = [ 50.0*j for j in range(1,21) ]
-master = open( "run.sh", "w" )
-tuples = open( "tuplelist.txt", "w" )
+#!/usr/bin/env python 
+import numpy
+import sys, getopt
+def main(argv):
+  g4particle="pi+"
+  plist='FTFP_BERT'
+  nsamples=10
+  nevts=10000
+  material="G4_C"
+  mine = 31
+  maxe=700000
+  #print 'Number of arguments:', len(sys.argv), 'arguments.'
+  #print 'Argument List:', str(sys.argv)
+  try:
+    opts, args = getopt.getopt(argv,"p:n:l:s:",["mat=","min=","max="])
+  except getopt.GetoptError:
+    print './makePi+Inputs.py  -p <particle> -n <nrofeevents>'
+    sys.exit(2)
 
-n = 0
-
-for run in energies:
-  out = open( "%ss_%s_%03d.in" % (particle,plist,n), "w" )
-
-  print >> out, "/control/verbose 1 "
-  print >> out, "/run/verbose 1 "
-  print >> out, "/tracking/verbose 0 "
-  print >> out, "/testhadr/Physics %s " % plist
-  print >> out, ""
-  print >> out, "/testhadr/TargetMat G4_C "
-  print >> out, "/testhadr/TargetRadius 100 cm "
-  print >> out, "/testhadr/TargetLength 1 cm "
-  print >> out, ""
-  print >> out, "/testhadr/Update "
-  print >> out, "/run/initialize "
-  print >> out, "/random/setSeeds %d %d " % (n, 31+n)
-  print >> out, ""
-  print >> out, "/gun/particle %s " % g4particle
-  print >> out, "/gun/energy %2.1f MeV " % run
-  print >> out, "/run/beamOn 1000000"
-
-  print >> master, "g4had.exe %ss_%s_%03d.in" % (particle,plist,n)
-  print >> tuples, "ntuple_%04d.root" % n
-
-  n += 1
-
+  for opt, arg in opts:
+    if opt == '-p':
+      g4particle= arg
+      print g4particle
+    if opt == '-n':
+      nevts = arg
+      print nevts
+    if opt == '-l':
+      plist= arg
+      print plist
+    if opt == '-s':
+      nsamples  = arg
+      print nsamples
+    if opt == '--mat':
+      material = arg
+    if opt == '--min':
+      mine = arg
+    if opt == '--max':
+      maxe = arg
+  master = open( "run.sh", "w" )
+  tuples = open( "tuplelist.txt", "w" )
+  print >> master,"export PHYSLIST=%s" % plist
+  n = 0
+  for energies in  numpy.exp(numpy.linspace(numpy.log(mine), numpy.log(maxe), nsamples)):
+    macroname =  "%s_%s_%s_%06d_MeV.in"   % (g4particle,material,plist,energies)
+    filename  =  "%s_%s_%s_%06d_MeV.root" % (g4particle,material,plist,energies)
+    out = open(macroname , "w" )
+    
+    print >> out, "/control/verbose 1 "
+    print >> out, "/run/verbose 1 "
+    print >> out, "/tracking/verbose 0 "
+    print >> out, "/testhadr/TargetMat %s" % material
+    print >> out, "/testhadr/TargetRadius 100 cm "
+    print >> out, "/testhadr/TargetLength 1 cm "
+    print >> out, "/testhadr/Update "
+    print >> out, "/testana/FileName %s" % filename
+    print >> out, "/run/initialize "
+    print >> out, "/random/setSeeds %d %d " % (n, 31+n)
+    print >> out, "/gun/particle %s " % g4particle
+    print >> out, "/gun/energy %2.1f MeV " % energies
+    print >> out, "/run/beamOn %s" % nevts
+    print >> master,"../bin/g4had ",macroname
+    print >> tuples, filename
+    
+    n += 1
+    
+if __name__ == "__main__":
+  main(sys.argv[1:])
