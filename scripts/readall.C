38d352ea7e75e7fa107f1d79a771f64e269078fa
Hans-Joachim Wenzel <wenzel@fnal.gov>
2019-02-21 12:05:46 -0600

    add labels, set limits, drop el and inel cross section for gammas...

--- scripts/readall.C
+++ scripts/readall.C
@@ -40,13 +40,16 @@ void readall()
      "XS_pi+_G4_Ar.out",
      "XS_proton_G4_Ar.out"};
   //
+  double ulimits[G4sets]={3500.,60.,3000.,800.,3000.,2000.,2000.,1200.};
   string  Particles[G4sets];
-
   TCanvas *c[G4sets];
   TGraph *grtot[G4sets];
   TGraph *grel[G4sets];
   TGraph *grinel[G4sets];
   string title[G4sets];
+  string titletot[G4sets];
+  string titleel[G4sets];
+  string titleinel[G4sets];
 
   string line;
   string sstr;
@@ -106,38 +109,47 @@ void readall()
       std::copy(inel.begin(), inel.end(), inelarr);
       double totarr[tot.size()];
       std::copy(tot.begin(), tot.end(), totarr);
-      title[i] = Particles[i]+" on Ar Cross section";
+      title[i]    = Particles[i]+" on Ar Cross sections";
+      titletot[i] = Particles[i]+" on Ar total hadronic Cross section";
+      titleel[i]  = Particles[i]+" on Ar total elastic Cross section";
+      titleinel[i]= Particles[i]+" on Ar total inelastic Cross section";
       c[i] = new TCanvas(Particles[i].c_str(),title[i].c_str(),200,10,700,500);
       c[i]->SetGrid();
       c[i]->GetFrame()->SetBorderSize(12);
       //c[i]->SetLogx();
-      c[i]->SetLogy();
+      //c[i]->SetLogy();
       grtot[i]=new TGraph(tot.size(),enarr,totarr);
-      grtot[i]->SetTitle(title[i].c_str());
+      grtot[i]->SetTitle(titletot[i].c_str());
       grtot[i]->SetMarkerColor(2);
       //      gr[i]->SetMarkerSize(2);
       grtot[i]->SetLineColor(2);
       grtot[i]->SetMarkerStyle(21);
       grtot[i]->GetXaxis()->SetLimits(10.,4000.);
+      //      grtot[i]->SetRange(0.,ulimits[i] ,"Y");
+      grtot[i]->SetMinimum(0.);
+      grtot[i]->SetMaximum(ulimits[i]);
+      grtot[i]->GetXaxis()->SetTitle("E_{kin} [MeV]");
+      grtot[i]->GetYaxis()->SetTitle("#sigma [mb]");
+
+      //grtot[i]->GetYaxis()->SetRange(0.,ulimits[i]);
       grtot[i]->Draw("AC*");
-      grel[i]=new TGraph(tot.size(),enarr,elarr);
-      grel[i]->SetTitle(title[i].c_str());
-      grel[i]->SetMarkerColor(3);
-      //      gr[i]->SetMarkerSize(2);
-      grel[i]->SetLineColor(3);
-      grel[i]->SetMarkerStyle(22);
-      grel[i]->GetXaxis()->SetLimits(10.,4000.);
-      grel[i]->Draw("SAME");
-      grinel[i]=new TGraph(tot.size(),enarr,inelarr);
-      grinel[i]->SetTitle(title[i].c_str());
-      grinel[i]->SetMarkerColor(4);
-      //      gr[i]->SetMarkerSize(2);
-      grinel[i]->SetLineColor(4);
-      grinel[i]->SetMarkerStyle(23);
-      //      grinel[i]->GetXaxis()->SetLimits(10.,4000.);
-      grinel[i]->Draw("SAME");
+      if (Particles[i].compare("gamma")!=0)
+	{
+	  grel[i]=new TGraph(tot.size(),enarr,elarr);
+	  grel[i]->SetTitle(titleel[i].c_str());
+	  grel[i]->SetMarkerColor(3);
+	  grel[i]->SetLineColor(3);
+	  grel[i]->SetMarkerStyle(22);
+	  grel[i]->Draw("SAME");
+	  grinel[i]=new TGraph(tot.size(),enarr,inelarr);
+	  grinel[i]->SetTitle(titleinel[i].c_str());
+	  grinel[i]->SetMarkerColor(4);
+	  grinel[i]->SetLineColor(4);
+	  grinel[i]->SetMarkerStyle(23);
+	  grinel[i]->Draw("SAME");
+	}
       //
-       TLegend *leg = c[i]->BuildLegend(.5, .1, 0.9, .3);
+      TLegend *leg = c[i]->BuildLegend(.5, .7, 0.85, .85);
       leg->Draw();
       tot.clear();
       en.clear();
